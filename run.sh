d8709518d1bc7a07da29e56dd35cdf8879f4af1e
Darren Klein <darren.a.klein@gmail.com>
2022-02-09 17:55:05 -0500

    Added args to bash script

--- run.sh
+++ run.sh
@@ -1,5 +1,40 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo -------- starting $1 stress test... --------
+# https://unix.stackexchange.com/questions/321126/dash-arguments-to-shell-scripts
+# usage() {
+#   cat << EOF >&2
+# Usage: $PROGNAME [-v] [-d <dir>] [-f <file>]
 
-eval env token=$2 wrk -t4 -c10 -d10s --latency http://localhost:4000/graph -s ./lua_scripts/$1.lua
+# -f <file>: ...
+#  -d <dir>: ...
+#        -v: ...
+# EOF
+#   exit 1
+# }
+
+auth_token=""
+script=""
+threads=0
+connections=0
+duration=0
+
+while getopts a:s:t:c:d: o; do
+  case $o in
+    (a) auth_token=$OPTARG;;
+    (s) script=$OPTARG;;
+    (t) threads=$OPTARG;;
+    (c) connections=$OPTARG;;
+    (d) duration=$OPTARG;;
+    # (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+
+echo -------- starting stress test... --------
+echo "script: $script"
+echo "auth_token: $auth_token"
+echo "threads: $threads"
+echo "connections: $connections"
+echo "duration: $duration"
+
+eval env token=$token wrk -t $threads -c $connections -d "$duration"s --latency http://localhost:4000/graph -s ./lua_scripts/$script.lua
